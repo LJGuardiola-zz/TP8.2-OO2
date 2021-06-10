9f8f9a70b2af66efb92e8c503e5c60aba83200c0
Lucas J. Guardiola <LJGuardiola@gmail.com>
2021-06-10 01:39:47 -0300

    Initial commit

---
+++ src/guardiola/utilization/Main.java
@@ -0,0 +1,9 @@
+package guardiola.utilization;
+
+import guardiola.framework.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        new Application().start();
+    }
+}
